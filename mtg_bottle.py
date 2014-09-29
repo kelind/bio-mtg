4571eff8cf7d28984ace00b84cd3ed295bba2e09
kelind <kalindbl@princeton.edu>
2014-09-28 23:23:56 -0400

    Begin adding card stat page

--- mtg_bottle.py
+++ mtg_bottle.py
@@ -50,21 +50,31 @@ def event_index(eventid):
     cursor.execute('''select participants.*, users.name, users.id as userid from participants left join users on participants.user_id = users.id where participants.tourney_id=? order by participants.rank''', (eventid,))
     return dict(eventid=eventid, cursor=cursor, date=date.strftime('%d/%m/%y'), info=tourney_info)
 
-@route('/users/userid>')
+@route('/users/<userid>')
 @view('user_index')
 def user_index(eventid):
     # Want a view of users
     # With links to tourneys played in and decks used
 
     return dict(eventid=eventid, cursor=cursor, date=date.strftime('%d/%m/%y'), info=tourney_info)
-# Want a view of individual cards
-# Tracking usage over time
-# Want to be able to visualize multiple cards on same graph
-# To compare usage...
+
+@route('/cards/<cardid>')
+@view('card_stats')
+def user_index(cardid):
+    # Want a view of individual cards
+    # Tracking usage over time
+    # Want to be able to visualize multiple cards on same graph
+    # To compare usage...
+
+    cursor.execute('''select count(*) as decks, sum(quantity) as total from decklists where card=?''', (cardid,))
+    stats = cursor.fetchone()
+
+    return dict(cardid=cardid, stats=stats)
 
 # Want a view of decks
 # Need to be able to identify decks first!
 # Want to be able to see deck usage over time
 # Want to be able to compare to other decks over same time period
+
 run(host='localhost', port=8000)
 con.close()
