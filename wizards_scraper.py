1389ffc3cf066ef835bf81c9e18bd08d72582e8b
kelind <kalindbl@princeton.edu>
2015-04-01 02:41:06 -0400

    Finish scraper

--- wizards_scraper.py
+++ wizards_scraper.py
@@ -17,6 +17,7 @@ def extract_bracket_info(tree):
 
     # We need to be able to look up seed from player name
     # As well as player name from seed
+    final_bracket['participants'] = {}
     inverse_bracket = {}
 
     # First, get the seeds for all players
@@ -27,7 +28,7 @@ def extract_bracket_info(tree):
         username = seed.xpath('.//text()')
         clean_seed = int(seednum[0][seednum[0].rfind('-') + 1 : ])
 
-        final_bracket[username[0]] = clean_seed
+        final_bracket['participants'][username[0]] = clean_seed
         inverse_bracket[clean_seed] = username[0]
 
     # Now, figure out the matchups and their results
@@ -41,8 +42,6 @@ def extract_bracket_info(tree):
         record = extract_win_loss(winner_data)
         winner = winner_data[ : winner_data.rfind(',')]
 
-        print winner, record
-
         # As far as I'm aware, only the top eight are ever shown in the bracket
         # (please please please don't let this assumption be wrong)
         # We can then handle this case-switch style
@@ -50,25 +49,25 @@ def extract_bracket_info(tree):
 
         if '1v8' in match_pairing:
             # Who won?
-            if final_bracket[winner] == 1:
+            if final_bracket['participants'][winner] == 1:
                 final_bracket['matchups']['1v8'] = [inverse_bracket[1], inverse_bracket[8], int(record[0]), int(record[1])]
             else:
                 final_bracket['matchups']['1v8'] = [inverse_bracket[8], inverse_bracket[1], int(record[0]), int(record[1])]
 
         elif '4v5' in match_pairing:
-            if final_bracket[winner] == 4:
+            if final_bracket['participants'][winner] == 4:
                 final_bracket['matchups']['4v5'] = [inverse_bracket[4], inverse_bracket[5], int(record[0]), int(record[1])]
             else:
                 final_bracket['matchups']['4v5'] = [inverse_bracket[5], inverse_bracket[4], int(record[0]), int(record[1])]
 
         elif '3v6' in match_pairing:
-            if final_bracket[winner] == 3:
+            if final_bracket['participants'][winner] == 3:
                 final_bracket['matchups']['3v6'] = [inverse_bracket[3], inverse_bracket[6], int(record[0]), int(record[1])]
             else:
                 final_bracket['matchups']['3v6'] = [inverse_bracket[6], inverse_bracket[3], int(record[0]), int(record[1])]
 
         elif '2v7' in match_pairing:
-            if final_bracket[winner] == 2:
+            if final_bracket['participants'][winner] == 2:
                 final_bracket['matchups']['2v7'] = [inverse_bracket[2], inverse_bracket[7], int(record[0]), int(record[1])]
             else:
                 final_bracket['matchups']['2v7'] = [inverse_bracket[7], inverse_bracket[2], int(record[0]), int(record[1])]
@@ -76,27 +75,27 @@ def extract_bracket_info(tree):
         # Below this point, pairings are AMBIGUOUS
         # due to the order of the code
         elif '8v4' in match_pairing:
-            final_bracket['matchups']['18v45'] = [inverse_bracket[final_bracket[winner]], 0, int(record[0]), int(record[1])]
+            final_bracket['matchups']['18v45'] = [inverse_bracket[final_bracket['participants'][winner]], 0, int(record[0]), int(record[1])]
 
         elif '7v3' in match_pairing:
-            final_bracket['matchups']['27v36'] = [inverse_bracket[final_bracket[winner]], 0, int(record[0]), int(record[1])]
+            final_bracket['matchups']['27v36'] = [inverse_bracket[final_bracket['participants'][winner]], 0, int(record[0]), int(record[1])]
 
         elif 'champion' in match_pairing:
-            final_bracket['matchups']['champion'] = [inverse_bracket[final_bracket[winner]], 0, int(record[0]), int(record[1])]
+            final_bracket['matchups']['champion'] = [inverse_bracket[final_bracket['participants'][winner]], 0, int(record[0]), int(record[1])]
 
     # Now that we've gone through the whole tree
     # assign the results of ambiguous matchups
-    if final_bracket[final_bracket['matchups']['18v45'][0]] in (1, 8):
+    if final_bracket['participants'][final_bracket['matchups']['18v45'][0]] in (1, 8):
         final_bracket['matchups']['18v45'][1] = final_bracket['matchups']['4v5'][0]
     else:
         final_bracket['matchups']['18v45'][1] = final_bracket['matchups']['1v8'][0]
 
-    if final_bracket[final_bracket['matchups']['27v36'][0]] in (2, 7):
+    if final_bracket['participants'][final_bracket['matchups']['27v36'][0]] in (2, 7):
         final_bracket['matchups']['27v36'][1] = final_bracket['matchups']['3v6'][0]
     else:
         final_bracket['matchups']['27v36'][1] = final_bracket['matchups']['2v7'][0]
 
-    if final_bracket[final_bracket['matchups']['champion'][0]] in (1, 4, 5, 8):
+    if final_bracket['participants'][final_bracket['matchups']['champion'][0]] in (1, 4, 5, 8):
         final_bracket['matchups']['champion'][1] = final_bracket['matchups']['27v36'][0]
     else:
         final_bracket['matchups']['champion'][1] = final_bracket['matchups']['18v45'][0]
@@ -124,10 +123,8 @@ def extract_event_info(tree):
 
     event_data['format'] = raw_infomatch.group('format')
     event_data['metagame'] = raw_infomatch.group('metagame')
-    event_data['eventid'] = raw_infomatch.group('eventid')
+    event_data['eventid'] = int(raw_infomatch.group('eventid'))
     event_data['eventdate'] = datetime.datetime.strptime(raw_infomatch.group('date'), '%m/%d/%Y')
-
-    print event_data
 
     # If this is a "premier" event, extract bracket info
     #bracket = extract_bracket_info(tree)
@@ -152,13 +149,12 @@ def extract_event_info(tree):
             event_data['players'][username]['wins'] = int(wins)
             event_data['players'][username]['losses'] = int(losses)
         except TypeError:
-            # This is a tournament with bracket information
-            # Win/loss is not directly given for each player
-            # We handle bracket data later
-            # (so we don't do it multiple times unnecessarily)
-            pass
-
-        print username, event_data['players'][username]
+            # Either this is a tournament with bracket information
+            # or one of those weird ones with no w/l info at all.
+            # To prevent problems with the latter, initialize
+            # win and loss to zero.
+            event_data['players'][username]['wins'] = 0
+            event_data['players'][username]['losses'] = 0
 
         # Get maindeck info
         main_cards = deck.xpath('.//div[@class="sorted-by-overview-container sortedContainer"]//span[@class="card-name"]/a/text()')
@@ -177,8 +173,10 @@ def extract_event_info(tree):
 
     # Get bracket data if present
     bracket = tree.xpath('id("content-detail-page-of-an-article")//div[@class="wiz-bracket"]/table/tbody')
-    event_data['bracket'] = extract_bracket_info(bracket)
-    total_rounds = 0
+
+    if bracket:
+        event_data['bracket'] = extract_bracket_info(bracket)
+        total_rounds = 0
 
     for entry in standings:
         player = entry.xpath('.//td/text()')
@@ -199,18 +197,15 @@ def extract_event_info(tree):
 
                 event_data['players'][player[1]]['wins'] = player_points / 3
                 event_data['players'][player[1]]['losses'] = total_rounds - player_points / 3
-                print player[1], event_data['players'][player[1]]['wins']
-                print player[1], event_data['players'][player[1]]['losses']
 
             event_data['players'][player[1]]['rank'] = int(player[0])
             event_data['players'][player[1]]['points'] = int(player[2])
             event_data['players'][player[1]]['omwp'] = float(player[3])
             event_data['players'][player[1]]['gwp'] = float(player[4])
             event_data['players'][player[1]]['ogwp'] = float(player[5])
-            #print player[1], event_data['players'][player[1]]
 
         except ValueError:
-            print 'valueerror'
+            # I don't remember why this is in here
             continue
 
         except KeyError:
@@ -222,9 +217,7 @@ def extract_event_info(tree):
                 print 'Unrecognized player data in standings'
                 break
 
-    '''
     return event_data
-    '''
 
 def extract_win_loss(str):
     '''
@@ -251,25 +244,14 @@ if __name__ == '__main__':
 
     from lxml import etree
     import requests
+    from sys import argv
 
     html_parser = etree.HTMLParser(encoding='UTF-8')
 
-    #r = requests.get('http://www.wizards.com/Magic/Digital/MagicOnlineTourn.aspx?x=mtg/digital/magiconline/tourn/6683928')
-    r = requests.get('http://www.wizards.com/Magic/Digital/MagicOnlineTourn.aspx?x=mtg/digital/magiconline/tourn/6683856')
-    #r = requests.get('http://www.wizards.com/Magic/Digital/MagicOnlineTourn.aspx?x=mtg/digital/magiconline/tourn/6800507')
+    r = requests.get(argv[1])
 
     tree = etree.fromstring(r.text, html_parser)
 
     event_data = extract_event_info(tree)
 
     print event_data
-
-    '''
-    r = requests.get('http://www.wizards.com/handlers/XMLListService.ashx?dir=mtgo&type=XMLFileInfo&start=7')
-
-    tourney_list = r.json()
-
-    for tourney in tourney_list:
-
-        print tourney['Hyperlink']
-    '''
